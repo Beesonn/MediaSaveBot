b5f0283d5ed302a18828641d7ee75e6ce5efb43d
Beeson <131391958+Beesonn@users.noreply.github.com>
2026-03-05 18:21:02 +0530

    Refactor Instagram media handling and cleanup
    Removed unused imports and refactored Instagram handling logic to simplify media processing.

--- utils/instagram.go
+++ utils/instagram.go
@@ -2,14 +2,8 @@ package utils
 
 import (
     "fmt"
-    "strings"
-    "time"
-    "context"
-    "os"
-    "strconv"
 
     "github.com/Beesonn/dlkitgo"
-    "github.com/Beesonn/MediaSaveBot/database"
     "github.com/PaulSonOfLars/gotgbot/v2"
     "github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
@@ -17,18 +11,9 @@ import (
 func HandleInstagram(b *gotgbot.Bot, ctx *ext.Context) error {
     url := ctx.EffectiveMessage.Text
     
-    postID := extractInstagramID(url)
-    
     statusMsg, err := ctx.EffectiveMessage.Reply(b, "Downloading.....", nil)
     if err != nil {
         return err
-    }
-
-    if postID != "" {
-        cachedMedia, err := database.GetMedia(context.Background(), "instagram", postID)
-        if err == nil && cachedMedia != nil && len(cachedMedia.FileIDs) > 0 {
-            return sendCachedInstagram(b, ctx, cachedMedia, statusMsg)
-        }
     }
 
     client := dlkitgo.NewClient()
@@ -46,58 +31,6 @@ func HandleInstagram(b *gotgbot.Bot, ctx *ext.Context) error {
         return err
     }
 
-    channelIDStr := os.Getenv("CHANNEL_ID")
-    var dbChannelID int64 = 0
-    if channelIDStr != "" {
-        dbChannelID, _ = strconv.ParseInt(channelIDStr, 10, 64)
-    }
-    
-    fileIDs := make([]string, 0)
-
-    if postID != "" && dbChannelID != 0 {
-        channelMedia := make([]gotgbot.InputMedia, 0)
-        
-        for _, source := range stream.Source {
-            if source.Type == "video" {
-                channelMedia = append(channelMedia, gotgbot.InputMediaVideo{
-                    Media:     gotgbot.InputFileByURL(source.URL),
-                    Caption:   stream.Caption,
-                    ParseMode: "HTML",
-                })
-            } else {
-                channelMedia = append(channelMedia, gotgbot.InputMediaPhoto{
-                    Media:     gotgbot.InputFileByURL(source.URL),
-                    Caption:   stream.Caption,
-                    ParseMode: "HTML",
-                })
-            }
-        }
-
-        sentMessages, err := b.SendMediaGroup(dbChannelID, channelMedia, nil)
-        if err == nil {
-            for _, msg := range sentMessages {
-                if msg.Photo != nil && len(msg.Photo) > 0 {
-                    fileIDs = append(fileIDs, msg.Photo[len(msg.Photo)-1].FileId)
-                } else if msg.Video != nil {
-                    fileIDs = append(fileIDs, msg.Video.FileId)
-                }
-            }
-            
-            if postID != "" && len(fileIDs) > 0 {
-                dbMedia := &database.Media{
-                    PostID:    postID,
-                    Platform:  "instagram",
-                    FileIDs:   fileIDs,
-                    Caption:   stream.Caption,
-                    Username:  stream.Username,
-                    MediaType: "mixed",
-                    CreatedAt: time.Now(),
-                }
-                database.SaveMedia(context.Background(), dbMedia)
-            }
-        }
-    }
-
     statusMsg.Delete(b, nil)
 
     media := make([]gotgbot.InputMedia, 0)
@@ -105,15 +38,11 @@ func HandleInstagram(b *gotgbot.Bot, ctx *ext.Context) error {
     for _, source := range stream.Source {
         if source.Type == "video" {
             media = append(media, gotgbot.InputMediaVideo{
-                Media:     gotgbot.InputFileByURL(source.URL),
-                Caption:   stream.Caption,
-                ParseMode: "HTML",
+                Media: gotgbot.InputFileByURL(source.URL),
             })
         } else {
             media = append(media, gotgbot.InputMediaPhoto{
-                Media:     gotgbot.InputFileByURL(source.URL),
-                Caption:   stream.Caption,
-                ParseMode: "HTML",
+                Media: gotgbot.InputFileByURL(source.URL),
             })
         }
     }
@@ -125,46 +54,4 @@ func HandleInstagram(b *gotgbot.Bot, ctx *ext.Context) error {
     })
 
     return err
-}
-
-func sendCachedInstagram(b *gotgbot.Bot, ctx *ext.Context, cachedMedia *database.Media, statusMsg *gotgbot.Message) error {
-    statusMsg.Delete(b, nil)
-
-    media := make([]gotgbot.InputMedia, 0)
-
-    for _, fileID := range cachedMedia.FileIDs {
-        if cachedMedia.MediaType == "video" {
-            media = append(media, gotgbot.InputMediaVideo{
-                Media:     gotgbot.InputFileByID(fileID),
-                Caption:   cachedMedia.Caption,
-                ParseMode: "HTML",
-            })
-        } else {
-            media = append(media, gotgbot.InputMediaPhoto{
-                Media:     gotgbot.InputFileByID(fileID),
-                Caption:   cachedMedia.Caption,
-                ParseMode: "HTML",
-            })
-        }
-    }
-
-    _, err := b.SendMediaGroup(ctx.EffectiveChat.Id, media, &gotgbot.SendMediaGroupOpts{
-        ReplyParameters: &gotgbot.ReplyParameters{
-            MessageId: ctx.EffectiveMessage.MessageId,
-        },
-    })
-
-    return err
-}
-
-func extractInstagramID(url string) string {
-    parts := strings.Split(url, "/")
-    for i, part := range parts {
-        if part == "p" || part == "reel" || part == "tv" {
-            if i+1 < len(parts) {
-                return strings.Split(parts[i+1], "?")[0]
-            }
-        }
-    }
-    return ""
 }
