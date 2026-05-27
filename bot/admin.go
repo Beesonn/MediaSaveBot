eed72872671837d0fd1c4ab19d417cfdea1285c3
Beeson <131391958+Beesonn@users.noreply.github.com>
2026-05-27 14:55:07 +0530

    Update admin.go

--- bot/admin.go
+++ bot/admin.go
@@ -1,14 +1,19 @@
 package bot
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"html"
 	"log"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
+	"regexp"
 
 	"github.com/Beesonn/MediaSaveBot/database"
 	"github.com/PaulSonOfLars/gotgbot/v2"
@@ -39,6 +44,29 @@ func init() {
 
 func isAdmin(userID int64) bool {
 	return adminIDs[userID]
+}
+
+func importsEval(code string) (string, string) {
+	code = strings.TrimSpace(code)
+
+	blockRe := regexp.MustCompile(`(?s)import\s*\((.*?)\)`)
+	if blockRe.MatchString(code) {
+		block := blockRe.FindString(code)
+		code = blockRe.ReplaceAllString(code, "")
+		return strings.TrimSpace(block), strings.TrimSpace(code)
+	}
+
+	singleRe := regexp.MustCompile(`(?i)import\s*\(?\s*"([^"]+)"\s*\)?`)
+	if singleRe.MatchString(code) {
+		importPath := singleRe.FindStringSubmatch(code)[1]
+
+		block := "import (\n\t\"" + importPath + "\"\n)"
+
+		code = singleRe.ReplaceAllString(code, "")
+		return block, strings.TrimSpace(code)
+	}
+
+	return "", strings.TrimSpace(code)
 }
 
 func Stats(b *gotgbot.Bot, ctx *ext.Context) error {
@@ -216,6 +244,113 @@ func HandleStopBroadcast(b *gotgbot.Bot, ctx *ext.Context) error {
 
 	_, err := query.Answer(b, &gotgbot.AnswerCallbackQueryOpts{
 		Text: "🛑 Broadcast stopping...",
+	})
+
+	return err
+}
+
+func EvalCmd(b *gotgbot.Bot, ctx *ext.Context) error {
+	if !isAdmin(ctx.EffectiveUser.Id) {
+		return nil
+	}
+
+	msg := ctx.EffectiveMessage
+
+	parts := strings.SplitN(msg.Text, " ", 2)
+	if len(parts) != 2 {
+		_, err := msg.Reply(b, "❌ Please provide code to evaluate.\nUsage: /eval <code>", &gotgbot.SendMessageOpts{
+			ParseMode: "HTML",
+		})
+		return err
+	}
+
+	codef := strings.TrimSpace(parts[1])
+
+	statusMsg, _ := msg.Reply(b, "🔄 Running code...", nil)
+
+	msgJSON, err := json.Marshal(msg)
+	if err != nil {
+		return nil
+	}
+
+	impts, code := importsEval(codef)
+
+	tmpFileName := fmt.Sprintf("eval_%d.go", time.Now().UnixNano())
+
+	botToken := os.Getenv("TOKEN")
+	if botToken == "" {
+		_, _, _ = statusMsg.EditText(b, "❌ Bot token not found in environment", nil)
+		return nil
+	}
+
+	fileContent := fmt.Sprintf(`package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/PaulSonOfLars/gotgbot/v2"
+)
+
+%s
+
+func main() {
+	b, err := gotgbot.NewBot("%s", nil)
+	if err != nil {
+		fmt.Println("Bot Error:", err)
+		return
+	}
+    
+	rawJSON := `+"`"+`%s`+"`"+`
+	m := new(gotgbot.Message)
+	if err := json.Unmarshal([]byte(rawJSON), m); err != nil {
+		fmt.Println("Unmarshal Error:", err)
+		return
+	}
+
+	var r *gotgbot.Message
+	if m.ReplyToMessage != nil {
+		r = m.ReplyToMessage
+	}
+
+	%s
+	
+	_ = b
+	_ = m
+	_ = r
+}
+`, impts, botToken, string(msgJSON), code)
+
+	if err := os.WriteFile(tmpFileName, []byte(fileContent), 0644); err != nil {
+		return nil
+	}
+	defer os.Remove(tmpFileName)
+
+	cmd := exec.Command("go", "run", tmpFileName)
+	var out, stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	runErr := cmd.Run()
+
+	output := out.String()
+	errOut := stderr.String()
+
+	res := fmt.Sprintf("<b>📝 Code:</b>\n<pre language='go'>%s</pre>\n\n", html.EscapeString(codef))
+
+	if runErr != nil || errOut != "" {
+		res += fmt.Sprintf("<b>❌ Error:</b>\n<pre language='go'>%s</pre>", html.EscapeString(errOut))
+	} else if output != "" {
+		res += fmt.Sprintf("<b>✅ Output:</b>\n<pre language='go'>%s</pre>", html.EscapeString(output))
+	} else {
+		res += "<b>✅ Success</b>"
+	}
+
+	if len(res) > 4000 {
+		res = res[:4000] + "\n\n... (truncated)"
+	}
+
+	_, _, err = statusMsg.EditText(b, res, &gotgbot.EditMessageTextOpts{
+		ParseMode: "HTML",
 	})
 
 	return err
