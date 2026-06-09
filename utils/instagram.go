04b624c97739aa270961b5464671532508ebfbf9
Beeson <131391958+Beesonn@users.noreply.github.com>
2026-06-09 22:43:38 +0530

    Update instagram.go

--- utils/instagram.go
+++ utils/instagram.go
@@ -48,12 +48,7 @@ func HandleInstagram(b *gotgbot.Bot, ctx *ext.Context) error {
 
 	media := make([]gotgbot.InputMedia, 0)
 
-	for _, s := range sources {
-		type MediaItem struct {
-			Type string
-			URL  string
-		}
-		source := s.(MediaItem)
+	for _, source := range sources {
 		if source.Type == "video" {
 			media = append(media, gotgbot.InputMediaVideo{
 				Media:   gotgbot.InputFileByURL(source.URL),
